62239b8aa935e6bb445065c4fdedb7747fb74f01
cananbuyukdagg <canan@dabnet.com.tr>
2024-05-13 21:34:45 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+function XoxGameComponent(){
+  return(
+    <>
+    <h1>XOX GAME</h1>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <XoxGameComponent/>
+);
+
+reportWebVitals();
